fb688b2bded3bcd19fc2594eb849d786aca359d6
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-24 10:20:15 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {FaLinkedin} from "react-icons/fa";
 import {FaGithub} from "react-icons/fa";
 
 const Navbar = () => {
-  return <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+  return <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
         <img src={logo} alt="logo" />
     </div>
