5b2ea076814c08df32ffc09f07481bb59087d115
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-24 11:41:42 +0100

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {HERO_CONTENT} from "../constants"
+import profilePic from "../assets/profilePicture.png";
 
 const Hero = () => {
   return (
@@ -10,6 +11,11 @@ const Hero = () => {
                 <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Atharva</h1>
                 <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">CS Student</span>
                 <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
+            </div>
+        </div>
+        <div className='w-full lg:w-1/2 lg:p-8'>
+            <div className='flex justify-center'>
+                <img src={profilePic} alt="profile picture" />
             </div>
         </div>
       </div>
