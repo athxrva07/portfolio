eed1f7cf7fa958bffcc5d7922194a6703f181e3a
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-24 09:54:45 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,6 @@
 import logo from "../assets/kevinRushLogo.png"
 import {FaLinkedin} from "react-icons/fa";
-import {FaLinkedin} from "react-icons/fa";
-import {FaLinkedin} from "react-icons/fa";
-import {FaLinkedin} from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
 
 const Navbar = () => {
   return <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
