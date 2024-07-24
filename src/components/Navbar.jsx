a8ea8a95898478e3f1ad68caae165a7b839f2909
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-24 09:53:16 +0100

    updates

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import logo from "../assets/kevinRushLogo.png"
+import {FaLinkedin} from "react-icons/fa";
+import {FaLinkedin} from "react-icons/fa";
+import {FaLinkedin} from "react-icons/fa";
+import {FaLinkedin} from "react-icons/fa";
+
+const Navbar = () => {
+  return <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+    <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" />
+    </div>
+  </nav>
+
+};
+
+export default Navbar
