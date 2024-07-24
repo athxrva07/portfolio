b1bed307539cf6cd978d79151edd6061266d7fb3
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-24 12:03:03 +0100

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,17 @@
+import aboutImg from "../assets/profilePicture.png";
+
 const About = () => {
   return <div className="border-b border-neutral-900 pb-4">
-    <h1 className="my-20 text-center text-4xl"></h1>
+    <h2 className="my-20 text-center text-4xl"> About
+        <span className="text-neutral-500"> Me</span>
+    </h2>
+    <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2 lg:p-8">
+            <div className="flex item-center justify-center">
+                <img  className="rounded-2xl" src={aboutImg} alt="about"/>
+            </div>
+        </div>
+    </div>
   </div>
   };
 
