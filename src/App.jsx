e818f22e29d7483a297bc02deebddd01d9908eeb
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-24 11:04:42 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Navbar from "./components/Navbar"
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
@@ -7,10 +8,11 @@ const App = () => {
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
 
       <div className="container mx-auto px-8">
-      <Navbar />
+        <Navbar />
+        <Hero />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
