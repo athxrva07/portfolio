613852203804e28b7724ea83f539b4d8c6e7393d
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-26 09:27:08 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import aboutImg from "../assets/about.png";
+import { ABOUT_TEXT } from "../constants"
 
 const About = () => {
   return <div className="border-b border-neutral-900 pb-4">
@@ -9,6 +10,11 @@ const About = () => {
         <div className="w-full lg:w-1/2 lg:p-8">
             <div className="flex item-center justify-center">
                 <img className="rounded-xl" src={aboutImg} alt="about"/>
+            </div>
+        </div>
+        <div className="w-full lg:w-1/2">
+            <div className="flex justify-center lg:justify-start">
+              <p className="my-5 maxw-xl py-6">{ABOUT_TEXT}</p>
             </div>
         </div>
     </div>
