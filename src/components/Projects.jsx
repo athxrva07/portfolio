65560cabdd412f4b9a86d5b101c753a48f54d032
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-26 11:58:19 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,21 @@
+import { PROJECTS } from "../constants";
+
 const Projects = () => {
   return (
     <div className="pb-4 border-b border-neutral-900">
       <h2 className="my-20 text-4xl text-center">Projects</h2>
+      <div className="">
+        {PROJECTS.map((project, index) => (
+            <div key={index} className="flex flex-wrap mb-8 lg:justify-center">
+                <div className="w-full lg:w-1/4">
+                  <img src={project.image} width={150} height={150} alt={project.title} className="mb-6 rounded"/>
+                </div>
+                <div className="w-full max-w-xl lg:w-3/4">
+                    <h6>{project.title}</h6>
+                </div>
+            </div>
+        ))}
+      </div>
     </div>
   );
 };
