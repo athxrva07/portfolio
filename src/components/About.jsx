3980e84ffb24203c6b9eb44d9ff5572c0729fed7
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-26 14:41:23 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import aboutImg from "../assets/about.png";
-import { ABOUT_TEXT } from "../constants"
+import { ABOUT_TEXT } from "../constants";
+import { motion } from "framer-motion";
 
 const About = () => {
   return <div className="pb-16 border-b border-neutral-900">
@@ -7,16 +8,25 @@ const About = () => {
         <span className="text-neutral-500"> ME</span>
     </h2>
     <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2 lg:p-8">
+        <motion.div 
+        whileInView={{opacity: 1, x: 0}}
+        initial={{opacity: 0, x: -100}}
+        transition={{duration: 0.5}}
+        className="w-full lg:w-1/2 lg:p-8">
             <div className="flex justify-center item-center">
                 <img className="rounded-xl" src={aboutImg} alt="about"/>
             </div>
-        </div>
-        <div className="w-full lg:w-1/2">
+        </motion.div>
+        
+        <motion.div 
+        whileInView={{opacity: 1, x: 0}}
+        initial={{opacity: 0, x: 100}}
+        transition={{duration: 0.5}}
+        className="w-full lg:w-1/2">
             <div className="flex justify-center lg:justify-start">
               <p className="max-w-xl py-6 my-8 font-light tracking-tighter">{ABOUT_TEXT}</p>
             </div>
-        </div>
+        </motion.div>
     </div>
   </div>
   };
