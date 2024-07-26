4b226358fdc69b9dbec0af73581dbd973270e45c
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-26 15:16:06 +0100

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -17,9 +17,15 @@ const Experience = () => {
         {EXPERIENCES.map((experience, index) => (
           <div 
             key={index} className="flex flex-wrap mb-8 lg:justify-center">
-             <div className="w-full lg:w-1/4">
+
+             <motion.div 
+              whileInView={{opacity: 1, x: 0}}
+              initial={{opacity: 0, x: -100}}
+              transition={{duration: 1}}
+              className="w-full lg:w-1/4">
                 <p className="mb-2 text-sm text-neutral-100">{experience.year}</p>
-             </div>
+             </motion.div>
+
              <div className="w-full max-w-xl lg:w-3/4">
                <h6 className="mb-2 font-semibold">
                  {experience.role} - {" "}
