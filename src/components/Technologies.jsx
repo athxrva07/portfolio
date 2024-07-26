6e0f5e6a96838e3e57963d52e0161cc9dccbd3e4
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-26 10:18:02 +0100

    Update Technologies.jsx

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,6 +1,6 @@
 import { RiReactjsLine } from "react-icons/ri";
 import { TbBrandMongodb } from "react-icons/tb";
-import { TbBrandNextjs } from "react-icons/tb";
+import { FaJava } from "react-icons/fa6";
 
 const Technologies = () => {
   return (
@@ -8,13 +8,13 @@ const Technologies = () => {
       <h2 className="my-20 text-4xl text-center">Technologies</h2>
       <div className="flex flex-wrap items-center justify-center gap-4">
         <div className="p-4 border-4 rounded-2xl border-neutral-800">
-            <RiReactjsLine className="text-7xl text-cyan-400"/>
+            <FaJava className="text-7xl text-cyan-400"/>
         </div>
         <div className="p-4 border-4 rounded-2xl border-neutral-800">
             <TbBrandMongodb className="text-7xl text-cyan-400"/>
         </div>
         <div className="p-4 border-4 rounded-2xl border-neutral-800">
-            <TbBrandNextjs className="text-7xl text-cyan-400"/>
+            <TbBrandMongodb className="text-7xl text-cyan-400"/>
         </div>
         <div className="p-4 border-4 rounded-2xl border-neutral-800">
             <RiReactjsLine className="text-7xl text-cyan-400"/>
