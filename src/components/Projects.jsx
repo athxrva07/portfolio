93c0b0517d9580c17f6042ddd25dea24a8c9cf7b
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-26 11:46:09 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,8 @@
 const Projects = () => {
   return (
-    <div className="pb-4 border-b border-neutral-900">Projects</div>
+    <div className="pb-4 border-b border-neutral-900">
+      <h2 className="my-20 text-4xl text-center">Projects</h2>
+    </div>
   );
 };
 
