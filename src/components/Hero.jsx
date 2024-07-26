d6829f14d6f1f8ee90e46321fdffaaf60d3e6653
Atharva <130344032+athxrva07@users.noreply.github.com>
2024-07-26 14:27:20 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,7 +42,11 @@ const Hero = () => {
         </div>
         <div className='w-full lg:w-1/2 lg:p-8'>
             <div className='flex justify-center'>
-                <img className="rounded-xl" src={profilePic} alt="profile picture" />
+                <motion.img 
+                initial={{x: 100, opacity: 0}}
+                animate={{x: 0, opacity: 1}}
+                transition={{duration: 1, delay: 1.2}}
+                className="rounded-xl" src={profilePic} alt="profile picture" />
             </div>
         </div>
       </div>
